c76a1133c9f386daada3ad3b750ea54a4f3ff220
Andreas Klamm <andi-klamm@t-online.de>
2017-09-01 10:11:28 +0200

    editfunktion auskommentiert

--- kanban/src/view/MainScreenController.java
+++ kanban/src/view/MainScreenController.java
@@ -60,26 +60,26 @@ public class MainScreenController {
      * Called when the user clicks the edit button. Opens a dialog to edit
      * details for the selected person.
      */
-    @FXML
-    private void handleEditPerson() {
-        Task selectedTask = todoTTV.getSelectionModel().getSelectedItem();
-        if (selectedTask != null) {
-            boolean okClicked = mainApp.showTaskEditDialog(selectedTask);
-            if (okClicked) {
-                showPersonDetails(selectedTask);
-            }
-
-        } else {
-            // Nothing selected.
-            Alert alert = new Alert(AlertType.WARNING);
-            alert.initOwner(mainApp.getPrimaryStage());
-            alert.setTitle("No Selection");
-            alert.setHeaderText("No Person Selected");
-            alert.setContentText("Please select a person in the table.");
-
-            alert.showAndWait();
-        }
-    }
+//    @FXML
+//    private void handleEditPerson() {
+//        Task selectedTask = todoTTV.getSelectionModel().getSelectedItem();
+//        if (selectedTask != null) {
+//            boolean okClicked = mainApp.showTaskEditDialog(selectedTask);
+//            if (okClicked) {
+//                showPersonDetails(selectedTask);
+//            }
+//
+//        } else {
+//            // Nothing selected.
+//            Alert alert = new Alert(AlertType.WARNING);
+//            alert.initOwner(mainApp.getPrimaryStage());
+//            alert.setTitle("No Selection");
+//            alert.setHeaderText("No Person Selected");
+//            alert.setContentText("Please select a person in the table.");
+//
+//            alert.showAndWait();
+//        }
+//    }
     
     // Reference to the main application.
     private MainApp mainApp;
