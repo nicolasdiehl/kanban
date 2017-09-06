ffe522caa4d8387fff678e31cc706aaf85e20d4b
Nicolas Diehl <nicolas.2.diehl@continental-corporation.com>
2017-09-06 23:32:36 +0200

    Added reactive send/listen message functionality.

--- kanbanserver/src/kanbanserver/KClient.java
+++ kanbanserver/src/kanbanserver/KClient.java
@@ -7,71 +7,107 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.util.ArrayList;
 
+import model.Project;
+import model.Task;
+
 public class KClient {
 
-    public static void main(String args[]) throws IOException {
+	static Socket socket = null;
+	static ObjectOutputStream objectOutputStream = null;
+	static ObjectInputStream objectInputStream = null;
 
-        String url = "";
-        int port = 0;
-        if (args.length == 2) {
-            url = args[0];
-            port = Integer.valueOf(args[1]);
-        } else {
-            throw new IllegalArgumentException("Argument one should be ip/url and argument two should be port.");
-        }
-        InetAddress address;
-        if (url.equals("local")) {
-            // InetAddress address = InetAddress.getLocalHost();
-            address = InetAddress.getByName("127.0.0.1");
-            System.out.println(address.toString());
-        } else {
-            address = InetAddress.getByName(url);
-        }
-        Socket socket = null;
-        ObjectOutputStream objectOutputStream = null;
-        ObjectInputStream objectInputStream = null;
-        
-        try {
-            socket = new Socket(address, port);
-            objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-            objectInputStream = new ObjectInputStream(socket.getInputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.err.print("IO exception at client!");
-        }
+	public static void requestSimpleProjects() {
+		try {
+			System.out.println("Client Message: Sending Request to get SimpleProject 's.");
+			objectOutputStream.writeObject("SimpleProjects");
+		} catch (IOException e) {
+			System.err.println("Client Error: sending requestSimpleProjects message.");
+			e.printStackTrace();
+		}
+	}
 
-        System.out.println("Client address:" + address);
-        System.out.println("Receiving objects from server.");
+	public static void main(String args[]) throws IOException {
 
-        try {
-            @SuppressWarnings("unchecked")
-			ArrayList<SimpleProject> returnedList = (ArrayList<SimpleProject>) objectInputStream.readObject();
-            System.out.println("received " + returnedList.size() + " SimpleObject objects.");
-            socket.close();
-        } catch(IOException | ClassNotFoundException | ClassCastException e) {
-        	e.printStackTrace();
-            System.out.println("Error reading socket!");
-        } finally {
-    			try {
-    				System.out.println("Client connection closing..");
-    				if (objectOutputStream != null) {
-    					objectOutputStream.close();
-    					System.out.println("objectOutputStream closed!");
-    				}
+		Boolean receivingMessages = true;
+		String url = "";
+		int port = 0;
+		if (args.length == 2) {
+			url = args[0];
+			port = Integer.valueOf(args[1]);
+		} else {
+			throw new IllegalArgumentException(
+					"Client Error: Argument one should be ip/url and argument two should be port.");
+		}
+		InetAddress address;
+		if (url.equals("local")) {
+			// InetAddress address = InetAddress.getLocalHost();
+			System.out.println("Client Message: Using localhost as IP");
+			address = InetAddress.getByName("127.0.0.1");
+			System.out.println("Client Message: IP = " + address.toString());
+		} else {
+			System.out.println("Client Message: Using " + url + " as IP");
+			address = InetAddress.getByName(url);
+		}
 
-    				if (objectInputStream != null) {
-    					objectInputStream.close();
-    					System.out.println("objectInputStream closed!");
-    				}
+		try {
+			socket = new Socket(address, port);
+			objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+			objectInputStream = new ObjectInputStream(socket.getInputStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("Client Message: IO exception at client connecting to server!");
+			System.exit(1);
+		}
 
-    				if (socket != null) {
-    					socket.close();
-    					System.out.println("Socket closed!");
-    				}
+		System.out.println("Client Message: Requesting list of Projects!");
+		requestSimpleProjects();
 
-    			} catch (IOException ie) {
-    				System.out.println("Error closing clients socket, objectOutputStream or objectInputStream.");
-    			}
-    		}
-    }
+		System.out.println("Client Message: Address:" + address);
+		System.out.println("Client Message: Receiving objects from server.");
+
+		Object currentObject = null;
+		while (receivingMessages) {
+			try {
+				currentObject = objectInputStream.readObject();
+				if (currentObject instanceof ArrayList<?>) {
+					System.out.println("Client Message: Received Object is ArrayList.");
+					if (((ArrayList<?>) currentObject).get(0) instanceof SimpleProject) {
+						System.out.println("Client Message: Objects are SimpleProjects.");
+						@SuppressWarnings("unchecked")
+						ArrayList<SimpleProject> returnedList = (ArrayList<SimpleProject>) currentObject;
+						System.out.println("received " + returnedList.size() + " SimpleObject objects.");
+					} else if (((ArrayList<?>) currentObject).get(0) instanceof Project) {
+						System.out.println("Client Message: Objects are SimpleProject 's.");
+						// do some other stuff
+					} else if (((ArrayList<?>) currentObject).get(0) instanceof Task) {
+						System.out.println("Client Message: Objects are Task 's.");
+						// do some other other stuff
+					}
+				}
+			} catch (IOException | ClassNotFoundException | ClassCastException e) {
+				e.printStackTrace();
+				System.err.println("Client Error: Error reading socket!");
+			}
+		}
+		try {
+			System.out.println("Client Message: Connection closing..");
+			if (objectOutputStream != null) {
+				objectOutputStream.close();
+				System.out.println("Client Message: ObjectOutputStream closed!");
+			}
+
+			if (objectInputStream != null) {
+				objectInputStream.close();
+				System.out.println("Client Message: objectInputStream closed!");
+			}
+
+			if (socket != null) {
+				socket.close();
+				System.out.println("Client Message: Socket closed!");
+			}
+
+		} catch (IOException ie) {
+			System.err.println("Client Error: Error closing clients socket, objectOutputStream or objectInputStream.");
+		}
+	}
 }
--- kanbanserver/src/kanbanserver/KServer.java
+++ kanbanserver/src/kanbanserver/KServer.java
@@ -10,6 +10,10 @@ import java.util.ArrayList;
 public class KServer {
 	public static ArrayList<SimpleProject> listOfObjects;
 
+	public static void requestSimpleProjects() {
+
+	}
+
 	public static void main(String args[]) {
 		listOfObjects = new ArrayList<SimpleProject>();
 		listOfObjects.add(new SimpleProject("name", "id"));
@@ -19,39 +23,38 @@ public class KServer {
 		if (args.length == 1) {
 			port = Integer.valueOf(args[0]);
 		} else {
-			throw new IllegalArgumentException("Argument one should be the port.");
+			throw new IllegalArgumentException("Server Error: Argument one should be the port.");
 		}
-		
+
 		Socket connectionSocket = null;
 		ServerSocket serverSocket = null;
-		System.out.println("Server waiting for connection on port " + port + ".");
+		System.out.println("Server Message: Waiting for connection on port " + port + ".");
 		try {
 			serverSocket = new ServerSocket(port);
 			while (true) {
 				try {
 					connectionSocket = serverSocket.accept();
-					System.out.println("Connection OK!");
+					System.out.println("Server Message: Connection OK!");
 					ServerThread serverThread = new ServerThread(connectionSocket);
 					serverThread.start();
-	
-				}
-				catch (Exception e) {
+
+				} catch (Exception e) {
 					e.printStackTrace();
-					System.out.println("Connection error!");
+					System.err.println("Server Error: Connection accept error!");
 				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
-			System.out.println("Server error!");
-		}
-		finally {
-			// Finally runs after the "endless while" for now, no need to stop the server.
+			System.err.println("Server Error: New Server socket Error!");
+		} finally {
+			// Finally runs after the "endless while" for now, no need to stop
+			// the server.
 			if (serverSocket != null) {
 				try {
 					serverSocket.close();
-				    System.out.println("serverSocket closed!");
+					System.out.println("Server Message: serverSocket closed!");
 				} catch (IOException ie) {
-					System.out.println("Error closing serverSocket.");
+					System.out.println("Server Error: Error closing serverSocket.");
 				}
 			}
 		}
@@ -60,49 +63,72 @@ public class KServer {
 
 class ServerThread extends Thread {
 
-    ObjectOutputStream objectOutputStream = null;
-    ObjectInputStream objectInputStream = null;
+	Boolean receivingMessages = true;
+	Object currentObject = null;
+	ObjectOutputStream objectOutputStream = null;
+	ObjectInputStream objectInputStream = null;
 	Socket socket = null;
 
 	public ServerThread(Socket socket) {
 		this.socket = socket;
 	}
 
+	public void sendSimpleProjects() {
+		try {
+			System.out.println("Server Message: Sending SimpleProject 's.");
+			objectOutputStream.writeObject(KServer.listOfObjects);
+		} catch (IOException e) {
+			System.err.println("Server Error: Error sending list of simple objects to client.");
+			e.printStackTrace();
+		}
+	}
+
 	public void run() {
 		try {
-		    objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-		    objectInputStream = new ObjectInputStream(socket.getInputStream());
+			objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+			objectInputStream = new ObjectInputStream(socket.getInputStream());
 
 		} catch (IOException e) {
-			System.out.println("IO error in server thread");
+			System.err.println("Server Error: IO error in server thread");
+		}
+
+		while (receivingMessages) {
+			try {
+				currentObject = objectInputStream.readObject();
+				if (currentObject instanceof String) {
+					System.out.println("Server Message: Received String Object");
+					if (currentObject.equals("SimpleProjects")) {
+						System.out.println("Server Message: Client is requesting SimpleProject 's");
+						sendSimpleProjects();
+					}
+				} else {
+					System.err.println("Server Error: Received object is not a String!?");
+				}
+			} catch (IOException | ClassNotFoundException | ClassCastException e) {
+				// e.printStackTrace();
+				// System.err.println("Error reading socket!");
+			}
 		}
 
 		try {
-			objectOutputStream.writeObject(KServer.listOfObjects);
-		} catch (IOException e) {
-            e.printStackTrace();
-            System.err.print("IO exception server!");
-		} finally {
-			try {
-				System.out.println("Server connection closing..");
-				if (objectOutputStream != null) {
-					objectOutputStream.close();
-					System.out.println("objectOutputStream closed!");
-				}
-
-				if (objectInputStream != null) {
-					objectInputStream.close();
-					System.out.println("objectInputStream closed!");
-				}
-
-				if (socket != null) {
-					socket.close();
-					System.out.println("Socket closed!");
-				}
-
-			} catch (IOException ie) {
-				System.out.println("Error closing servers socket, objectOutputStream or objectInputStream.");
+			System.out.println("Server Message: Server connection closing..");
+			if (objectOutputStream != null) {
+				objectOutputStream.close();
+				System.out.println("Server Message: ObjectOutputStream closed!");
 			}
+
+			if (objectInputStream != null) {
+				objectInputStream.close();
+				System.out.println("Server Message: objectInputStream closed!");
+			}
+
+			if (socket != null) {
+				socket.close();
+				System.out.println("Server Message: Socket closed!");
+			}
+
+		} catch (IOException ie) {
+			System.out.println("Server Error: Error closing Server socket, objectOutputStream or objectInputStream.");
 		}
 	}
 }
