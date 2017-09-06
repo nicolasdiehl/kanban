053b16ec0e9776e8120a57ae0663a330a977a7d8
Nicolas Diehl <nicolas.2.diehl@continental-corporation.com>
2017-09-06 09:39:52 +0200

    Renamed SimpleObject to SimpleProject (2)

--- kanbanserver/src/kanbanserver/SimpleProject.java
+++ kanbanserver/src/kanbanserver/SimpleProject.java
@@ -2,12 +2,12 @@ package kanbanserver;
 
 import java.io.Serializable;
 
-public class SimpleObject implements Serializable{
+public class SimpleProject implements Serializable{
 	private static final long serialVersionUID = -2398503995120490647L;
 	String name;
 	String id;
 
-	public SimpleObject(String name, String id) {
+	public SimpleProject(String name, String id) {
 		this.name = name;
 		this.id = id;
 	}
