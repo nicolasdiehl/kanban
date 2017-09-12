4d3b49e8d3d69aac575212ba719f482679951421
Nicolas Diehl <nicolas.2.diehl@continental-corporation.com>
2017-09-12 19:50:30 +0200

    Save received ArrayList of simpleProject 's in ClientControl.receivedSimpleObjects

--- kanban/src/control/ClientControl.java
+++ kanban/src/control/ClientControl.java
@@ -10,6 +10,7 @@ public class ClientControl {
 	private static ClientControl instance = new ClientControl();
 	private IProject openProject = new Project("title", "description", "me");
 	private List<IProject> sleepingProjects;
+	private ArrayList<SimpleProject> receivedSimpleProjects = new ArrayList<SimpleProject>();
 	private MainApp mainApp;
 	private Thread clientThread;
 	private KClient kclient;
@@ -77,5 +78,13 @@ public class ClientControl {
 
 	public void setOpenProject(IProject openProject) {
 	    this.openProject = openProject;
+	}
+
+	public ArrayList<SimpleProject> getReceivedSimpleProjects() {
+		return receivedSimpleProjects;
+	}
+
+	public void setReceivedSimpleProjects(ArrayList<SimpleProject> receivedSimpleProjects) {
+		this.receivedSimpleProjects = receivedSimpleProjects;
 	}
 }
