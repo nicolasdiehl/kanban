9e505c41446d0bb15a4539e81582ccb9d5e23bf0
SeboG <sebastian.gimkiewicz@gmx.de>
2017-09-12 14:26:40 +0200

    User ist jetzt Serializable auf KanBanServer

--- kanbanserver/src/model/User.java
+++ kanbanserver/src/model/User.java
@@ -1,9 +1,12 @@
 package model;
 
+import java.io.Serializable;
 import java.util.*;
 
-public class User
+public class User implements Serializable
 {
+	private static final long serialVersionUID = -2398524575120480637L;
+	
 	private String uid;
 	private String name;
 	private String pw;
