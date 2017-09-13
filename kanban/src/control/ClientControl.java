cea033a9b07ccd6f3b2c0d38121af0210f8b63a7
Chris-PC\Chris <janchristianhopp@gmail.com>
2017-09-13 09:22:30 +0200

    cuurent project for user gets set in ClientControl after creating a new one in projectscreencontroll

--- kanban/src/control/ClientControl.java
+++ kanban/src/control/ClientControl.java
@@ -78,6 +78,7 @@ public class ClientControl {
 	public boolean projectCreate(Project projectNew) {
 		boolean isProjectCreated = false;
 		//anlegen der Projekt-XML
+		user.setProjectCurrent(projectNew);
 		kclient.sendProject(projectNew);
 		
 		return isProjectCreated;
