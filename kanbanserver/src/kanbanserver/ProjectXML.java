49b07d7beae72ad4dc0596318cd78443e7ee2b26
Norman <norman.dettmer@gmx.de>
2017-09-13 08:40:25 +0200

    Added a function to read a project XML

--- kanbanserver/src/kanbanserver/ProjectXML.java
+++ kanbanserver/src/kanbanserver/ProjectXML.java
@@ -3,7 +3,9 @@ package kanbanserver;
 import java.io.File;
 import java.io.IOException;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -16,6 +18,7 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 import model.ITask;
 import model.Project;
@@ -31,38 +34,64 @@ public class ProjectXML
 		projectXMLDirectory = directory + pID + ".xml";
 	}
 
-	public Project readProjectXML(String directory, String loginName)
+	public Project readProjectXML()
 	{
-		// create new Directory of the project xml
-		String projectXMLDirectory = directory + loginName + ".xml";
-
-		// create object of class project, returning at the end of the function
-		Project projectRead = new Project(null, null, null, null, null, null, null);
-
-		// check if xml file exists
-		File file = new File(projectXMLDirectory);
-		if (file.exists())
-		{
-			System.out.println("XML wurde gefunden");
-		} else
-		{
-			System.out.println("XML wurde NICHT gefunden");
-		}
-
 		try
 		{
-			//
-			File fXmlFile = new File(projectXMLDirectory);
-			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder builder = factory.newDocumentBuilder();
-			Document doc = builder.parse(fXmlFile);
+			// essentiall to cast a string into a date
+			DateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 			
+			// create new file for import
+			File file = new File(projectXMLDirectory);
 			
+			// check if xml file exists
+			if (file.exists())
+			{
+				// Debugging output
+				System.out.println("XML wurde gefunden");
 
-			
-			
-			// normalize the XML Structure; >>recommended<<
-			doc.getDocumentElement().normalize();
+				// create new object of type document with the data of the xml
+				File fXmlFile = new File(projectXMLDirectory);
+				DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder builder = factory.newDocumentBuilder();
+				Document doc = builder.parse(fXmlFile);
+
+				// normalize the xml structure in the doc object; >>recommended<<
+				doc.getDocumentElement().normalize();
+
+				// Nodelist to store every element in root element
+				NodeList nList = doc.getElementsByTagName("data");
+
+				// get user id and store it as a string
+				NodeList projectData = doc.getElementsByTagName("project");
+				Element pData = (Element) projectData.item(0);
+				String pID = pData.getAttribute("pID");
+
+				// get elements 
+				Element o = (Element) nList.item(0);
+
+				// get project name
+				String pName = o.getElementsByTagName("pName").item(0).getTextContent().trim();
+				
+				// get project description
+				String pDescription = o.getElementsByTagName("pDescription").item(0).getTextContent().trim();
+						
+				// get modified date
+				String pMDate = o.getElementsByTagName("mDate").item(0).getTextContent().trim();
+				Date mDate = df.parse(pMDate);
+				
+				// get date of creation
+				String pCDate = o.getElementsByTagName("cDate").item(0).getTextContent().trim();
+				Date cDate = df.parse(pCDate);
+
+				// create object of class project, returning at the end of the function
+				return new Project(pName, pDescription, mDate, cDate, null, null, pID);
+				
+			} else
+			{
+				// Debugging output
+				System.out.println("XML wurde NICHT gefunden");
+			}
 
 		} catch (ParserConfigurationException pce)
 		{
@@ -73,10 +102,16 @@ public class ProjectXML
 		} catch (org.xml.sax.SAXException e)
 		{
 			e.printStackTrace();
+		} catch (ParseException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
-		return projectRead;
+		// return null if file wasn't found
+		return null;
 	}
+
 	public boolean writeProjectXML(Project project)
 	{
 		boolean isWritten = false;
@@ -93,6 +128,7 @@ public class ProjectXML
 
 			//Debugging output
 			//System.out.println("Alte XML wurde gelöscht");
+
 		}
 
 		try
@@ -137,67 +173,75 @@ public class ProjectXML
 			String reportCDate = df.format(project.getCreated());
 			cDate.appendChild(doc.createTextNode(reportCDate));
 			projectXML.appendChild(cDate);
-			
+
 			// store all authorized projects in a list of type SimpleProject
 			List<ITask> taskList = project.getTasks();
-			
+
 			// loop to store every task in xml file
 			for (int i = 0; i < taskList.size(); i++)
 			{
 				// add new element "task" to project
 				Element task = doc.createElement("task");
 				projectXML.appendChild(task);
+				
 				// add attribute title to task
 				task.setAttribute("tName", taskList.get(i).getTitle());
+				
 				// add new element "description" to task
 				Element tDescription = doc.createElement("tDescription");
 				tDescription.appendChild(doc.createTextNode(taskList.get(i).getDescription()));
 				task.appendChild(tDescription);
+				
 				// add new element "categorie" to task
 				Element tCategorie = doc.createElement("tCategorie");
 				tCategorie.appendChild(doc.createTextNode(taskList.get(i).getCategorie()));
 				task.appendChild(tCategorie);
+				
 				// add new element "status" to task
 				Element tStatus = doc.createElement("tStatus");
 				tStatus.appendChild(doc.createTextNode(taskList.get(i).getStatus()));
 				task.appendChild(tStatus);
+				
 				// add new element "creatorID" to task
 				Element tCreatorID = doc.createElement("tCreatorID");
 				tCreatorID.appendChild(doc.createTextNode(taskList.get(i).getCreatorID()));
 				task.appendChild(tCreatorID);
+				
 				// add new element "lastCall" to task
 				Element tLastCall = doc.createElement("tLastCall");
+				
 				// cast Date to String
 				String reportLastCall = df.format(taskList.get(i).getLastCall());
 				tLastCall.appendChild(doc.createTextNode(reportLastCall));
 				task.appendChild(tLastCall);
+				
 				// add new element "dateCreate" to task
 				Element tDateCreate = doc.createElement("tDateCreate");
+				
 				// cast Date to String
 				String reportDateCreate = df.format(taskList.get(i).getCreatorDate());
 				tDateCreate.appendChild(doc.createTextNode(reportDateCreate));
 				task.appendChild(tDateCreate);
-							
+
 				// loop to store the memberList in project xml
-				
-				
-//				// add new element "comment" to task
-//				Element tComment = doc.createElement("tComment");
-//				task.appendChild(tComment);
-//				
-//				// store all comments in a list of type String
-//				List<String> comments = taskList.get(i).getComment();
-//				// loop to store the commentlines in comment
-//				for (int d = 0; d < comments.size(); d++)
-//				{
-//					// add new element "commentline" to comment
-//					Element tCommentLine = doc.createElement("tCommentLine");
-//					
-//					tComment.appendChild(tCommentLine);
-//					
-//				}
-//				
-//				// add new element "List<String> comment" to task
+
+				//				// add new element "comment" to task
+				//				Element tComment = doc.createElement("tComment");
+				//				task.appendChild(tComment);
+				//				
+				//				// store all comments in a list of type String
+				//				List<String> comments = taskList.get(i).getComment();
+				//				// loop to store the commentlines in comment
+				//				for (int d = 0; d < comments.size(); d++)
+				//				{
+				//					// add new element "commentline" to comment
+				//					Element tCommentLine = doc.createElement("tCommentLine");
+				//					
+				//					tComment.appendChild(tCommentLine);
+				//					
+				//				}
+				//				
+				//				// add new element "List<String> comment" to task
 			}
 
 			// write the content into xml file
@@ -208,7 +252,6 @@ public class ProjectXML
 
 			transformer.transform(source, result);
 
-			System.out.println("Neue Project XMl wurde angelegt");
 			isWritten = true;
 
 		} catch (ParserConfigurationException pce)
